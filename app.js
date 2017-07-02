b67172ad3f8226ad67ff9f140320798d38e3a4e6
Martin Cervantes <cervantes.martine@gmail.com>
2017-07-02 08:11:29 -0600

    effects

---
+++ app.js
@@ -0,0 +1,30 @@
+$(function(){
+   $('.list-group a').click(function(e) {
+       e.preventDefault()
+
+        $that = $(this);
+        
+        $that.parent().find('a').removeClass('active');
+        $that.addClass('active');
+    });
+
+    $("#basic").click(function(){
+        $("#basic_list").slideToggle("slow");
+    });
+
+    $("#inter").click(function(){
+        $("#inter_list").slideToggle("slow");
+    });
+
+    $("#advanced").click(function(){
+        $("#advanced_list").slideToggle("slow");
+    });
+
+    $("#inter_p").click(function(){
+        $("#inter_projects").slideToggle("slow");
+    });
+
+    $("#advanced_p").click(function(){
+        $("#advanced_projects").slideToggle("slow");
+    });
+});
