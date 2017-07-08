16add08797fd451830d4930eec698ab4087ae78e
Martin Cervantes <cervantes.martine@gmail.com>
2017-07-08 08:09:23 -0600

    capitalize word

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
 $(function(){
     $("#basic").click(function(){
+        $(".active").next().slideToggle("slow");
         $("#basic_list").slideToggle("slow");
     });
 
     $("#inter").click(function(){
+        $(".active").next().slideToggle("slow");
         $("#inter_list").slideToggle("slow");
     });
 
     $("#advanced").click(function(){
+        $(".active").next().slideToggle("slow");
         $("#advanced_list").slideToggle("slow");
     });
 
     $("#inter_p").click(function(){
+        $(".active").next().slideToggle("slow");
         $("#inter_projects").slideToggle("slow");
     });
 
     $("#advanced_p").click(function(){
+        $(".active").next().slideToggle("slow");
         $("#advanced_projects").slideToggle("slow");
     });
 
